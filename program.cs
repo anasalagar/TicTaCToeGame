394481d60f59ec33dd38e6788a7031c773c2fa4f
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-11-17 12:53:45 +0530

    Anas - Added : Input From Player Taken

---
+++ program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Tic_Tac_Toe_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TicTacToeGame obj = new TicTacToeGame();
+            o.gameBoard();
+            o.abillityToInput();
+        }
+    }
+}
